e7c6ab20ce9fb351278b4a24bc4f693138c5f3cb
Khan Bifov <irestone.work@gmail.com>
2019-04-26 10:07:35 +0300

    Use sessions w/ Redis store

--- src/server/config.ts
+++ src/server/config.ts
@@ -1,23 +1,32 @@
 import { ConnectionOptions } from 'typeorm'
+import { SessionOptions } from 'express-session'
+import { RedisStoreOptions } from 'connect-redis'
 
 import * as entities from './entities'
 
 const {
-  NODE_ENV = 'development',
   HOST = 'localhost',
+  NODE_ENV = 'development',
+  ORM_DATABASE = 'playground',
+  ORM_HOST = 'localhost',
+  ORM_PASSWORD = 'postgres',
+  ORM_PORT = '5432',
+  ORM_USERNAME = 'postgres',
   PORT = '8080',
-  TYPEORM_HOST = 'localhost',
-  TYPEORM_PORT = '5432',
-  TYPEORM_USERNAME = 'postgres',
-  TYPEORM_PASSWORD = 'postgres',
-  TYPEORM_DATABASE = 'playground',
+  REDIS_HOST,
+  REDIS_PASS,
+  REDIS_PORT,
   SALT = '12',
+  SESS_LIFETIME = '3600000',
+  SESS_NAME = 'sid',
+  SESS_SECRET = 'secret',
 } = process.env
 
 // /////////////////////////////////////////////////////////////////////////////
 // ENVIRONMENT
 
 export const env: string = NODE_ENV
+export const inProduction: boolean = env === 'production'
 
 export const host: string = HOST
 export const port: number = parseInt(PORT)
@@ -25,16 +34,43 @@ export const port: number = parseInt(PORT)
 export const salt: number = parseInt(SALT)
 
 // /////////////////////////////////////////////////////////////////////////////
-// DB CONNECTIONS
+// DATA STORAGE
 
-export const typeorm: ConnectionOptions = {
+// DOMAIN
+
+// Postgress ORM
+export const ormConnectionOptions: ConnectionOptions = {
   type: 'postgres',
-  host: TYPEORM_HOST,
-  port: parseInt(TYPEORM_PORT),
-  username: TYPEORM_USERNAME,
-  password: TYPEORM_PASSWORD,
-  database: TYPEORM_DATABASE,
+  host: ORM_HOST,
+  port: parseInt(ORM_PORT),
+  username: ORM_USERNAME,
+  password: ORM_PASSWORD,
+  database: ORM_DATABASE,
   synchronize: true,
   logging: false,
   entities: Object.values(entities),
+}
+
+// SESSION
+
+// Sessions and cookies
+export const sessionOptions: SessionOptions = {
+  name: SESS_NAME,
+  secret: SESS_SECRET,
+  saveUninitialized: false,
+  resave: true,
+  rolling: true,
+  cookie: {
+    sameSite: true,
+    httpOnly: true,
+    secure: inProduction,
+    maxAge: parseInt(SESS_LIFETIME),
+  },
+}
+
+// Redis session store
+export const redisStoreOptions: RedisStoreOptions = {
+  host: REDIS_HOST,
+  port: (REDIS_PORT && parseInt(REDIS_PORT)) || undefined,
+  pass: REDIS_PASS,
 }
--- src/server/index.ts
+++ src/server/index.ts
@@ -1,50 +1,88 @@
 import 'reflect-metadata'
-import express from 'express'
+import express, { Application } from 'express'
 import { ApolloServer } from 'apollo-server-express'
 import { buildSchema } from 'type-graphql'
 import { createConnection, Connection } from 'typeorm'
+import expressSession from 'express-session'
+import connectRedis, { RedisStore as IRedisStore } from 'connect-redis'
 
-import { port, host, typeorm } from './config'
 import * as resolvers from './resolvers'
+import {
+  port,
+  host,
+  inProduction,
+  ormConnectionOptions,
+  sessionOptions,
+  redisStoreOptions,
+} from './config'
+
+interface IServerInfo {
+  links: string
+}
 
 class Server {
-  public static async bootstrap(): Promise<string> {
-    const connection: Connection = await createConnection(typeorm)
-    console.log(
-      `\n${
-        connection.isConnected
-          ? 'Connected to PostgreSQL'
-          : 'Connection to PostgreSQL failed'
-      }`
-    )
+  private static app: Application
+  private static links: { [name: string]: string } = {}
 
-    const schema = await buildSchema({
-      resolvers: Object.values(resolvers),
+  public static async launch(): Promise<IServerInfo> {
+    await Server.connectToDB()
+    await Server.createApp()
+    await Server.createSessionStore()
+    await Server.createApolloServer()
+    await Server.startListening()
+    return Server.createInfo()
+  }
+
+  private static async connectToDB(): Promise<void> {
+    const connection: Connection = await createConnection(ormConnectionOptions)
+    if (!connection.isConnected) throw new Error('DB connection failed')
+  }
+
+  private static async createApp(): Promise<void> {
+    Server.app = express()
+  }
+
+  private static async createSessionStore(): Promise<void> {
+    const RedisStore: IRedisStore = connectRedis(expressSession)
+    sessionOptions.store = new RedisStore(redisStoreOptions)
+    Server.app.use(expressSession(sessionOptions))
+  }
+
+  private static async createApolloServer(): Promise<void> {
+    const apollo = new ApolloServer({
+      schema: await buildSchema({ resolvers: Object.values(resolvers) }),
+      context: ({ req, res }): object => ({ req, res }),
+      playground: !inProduction && {
+        settings: { 'request.credentials': 'include' } as any,
+      },
     })
+    apollo.applyMiddleware({ app: Server.app, cors: false })
+    Server.links.GraphQL = `http://${host}:${port}${apollo.graphqlPath}`
+  }
 
-    const apollo = new ApolloServer({ schema })
-
-    const app = express()
-
-    apollo.applyMiddleware({ app })
-
-    // app.use((req: object, res: object): void => res.send(new Date()))
-
+  private static async startListening(): Promise<void> {
     await new Promise(
       (resolve): void => {
-        app.listen(port, (): void => resolve())
+        Server.app.listen(
+          port,
+          (): void => {
+            resolve()
+            Server.links.App = `http://${host}:${port}`
+          }
+        )
       }
     )
+  }
 
-    const links: [string, string][] = [
-      ['Server', `http://${host}:${port}`],
-      ['GraphQL', `http://${host}:${port}${apollo.graphqlPath}`],
-    ]
+  private static createInfo(): IServerInfo {
+    const links = Object.entries(Server.links)
+      .map(([name, url]): string => `${name}: ${url}`)
+      .join(`\n`)
 
-    return links.map(([name, url]): string => `${name}: ${url}`).join(`\n`)
+    return { links }
   }
 }
 
-Server.bootstrap()
-  .then((message: string): void => console.log(message))
-  .catch((err: object): void => console.error(err))
+Server.launch()
+  .then(({ links }: IServerInfo): void => console.log(links))
+  .catch((err: Error): void => console.error(err))
