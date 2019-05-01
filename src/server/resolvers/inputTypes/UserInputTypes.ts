c77e69c76df0f02000ec907786889fa92aa43c74
Khan Bifov <irestone.work@gmail.com>
2019-05-01 14:58:29 +0300

    Move input types to resolvers root

---
+++ src/server/resolvers/inputTypes/UserInputTypes.ts
@@ -0,0 +1,48 @@
+import { InputType, Field } from 'type-graphql'
+import { Length, IsEmail, MinLength } from 'class-validator'
+
+import { IsEmailUnique } from './UserInputTypes/IsEmailUnique'
+
+const minPasswordLength = 3
+
+// ====={ Register }
+
+@InputType()
+export class RegisterInput {
+  @Field()
+  @IsEmail()
+  @IsEmailUnique({ message: 'Email $value is already in use' })
+  public email: string
+
+  @Field()
+  @Length(1, 100)
+  public username: string
+
+  @Field()
+  @MinLength(minPasswordLength)
+  public password: string
+}
+
+// ====={ Login }
+
+@InputType()
+export class LoginInput {
+  @Field()
+  public username: string
+
+  @Field()
+  @MinLength(minPasswordLength)
+  public password: string
+}
+
+// ====={ Change Password }
+
+@InputType()
+export class ChangePasswordInput {
+  @Field()
+  public token: string
+
+  @Field()
+  @MinLength(minPasswordLength)
+  public newPassword: string
+}
